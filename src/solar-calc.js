62d1694305106a909ab9364ba2d39273d15401a0
Josh <Joshua.horgen@gmail.com>
2019-06-28 10:59:32 -0700

    Fixed webpack

--- src/solar-calc.js
+++ src/solar-calc.js
@@ -8,3 +8,17 @@ export class CalculateDay {
     return weekDays[userDate.getDay()]
   }
 }
+
+
+// calculateAgeInYears (date) {
+//   var userDate = new Date(this.date);
+//   var currentYear = userDate.getFullYear();
+//   var yearDiff = currentYear - date.getFullYear();
+//   var birthday = new Date(currentYear, date.getMonth(), date.getDate());
+//   var resultDate = (userDate >= birthday);
+//
+//   ? yearDiff
+//   : yearDiff - 1;
+//   return resultDate
+
+// }
